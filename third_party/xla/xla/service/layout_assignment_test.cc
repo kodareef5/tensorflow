af0d751e298629b1b60a51e0e052e2a6e486c76a
Blake Hechtman <blakehechtman@google.com>
2026-03-19 19:33:28 -0700

    [XLA:LAYOUT_ASSIGNMENT] Use alias analysis for layout assignment
    PiperOrigin-RevId: 886505149

--- third_party/xla/xla/service/layout_assignment_test.cc
+++ third_party/xla/xla/service/layout_assignment_test.cc
@@ -41,8 +41,8 @@ limitations under the License.
 #include "xla/literal.h"
 #include "xla/literal_util.h"
 #include "xla/service/computation_layout.h"
+#include "xla/service/hlo_value.h"
 #include "xla/service/hlo_verifier.h"
-#include "xla/service/logical_buffer.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/shape.h"
 #include "xla/shape_layout.h"
@@ -526,7 +526,7 @@ class OperandsMustBeTheSameLayoutAssignment : public LayoutAssignment {
   absl::Status PropagateBufferConstraint(
       const BufferLayoutConstraint& buffer_constraint,
       LayoutConstraints* constraints) override {
-    const LogicalBuffer& buffer = buffer_constraint.buffer();
+    const HloValue& buffer = buffer_constraint.buffer();
     const HloInstruction* instruction = buffer.instruction();
 
     // Force the operands' layout to the output layout.
@@ -1613,65 +1613,7 @@ ENTRY entry_computation {
   EXPECT_TRUE(abs->shape().is_dynamic_dimension(1));
 }
 
-// Test the ability to avoid copying across computations by reversing
-// computation traversal order.
-TEST_F(LayoutAssignmentTest, ReverseComputationOrderAvoidCopy) {
-  const char* module_str = R"(
-HloModule ComputationLayoutAvoidCopy
 
-call_1 {
-  %arg_tuple.1 = (f32[93184,4]) parameter(0)
-  %get-tuple-element.1 = f32[93184,4] get-tuple-element(%arg_tuple.1), index=0
-  ROOT %reshape.8494 = f32[2,512,364]{2,1,0} reshape(f32[93184,4]{1,0}%get-tuple-element.1)
-}
-
-on_true {
-  %arg_tuple.1 = (f32[93184,4]) parameter(0)
-  %get-tuple-element.1 = f32[93184,4] get-tuple-element(%arg_tuple.1), index=0
-  ROOT %reshape.8493 = f32[2,512,364]{2,1,0} reshape(f32[93184,4]{1,0}%get-tuple-element.1)
-}
-
-on_false {
-  %arg_tuple.2 = (f32[93184,4]) parameter(0)
-  %get-tuple-element.3 = f32[93184,4] get-tuple-element(%arg_tuple.2), index=0
-  %reshape.9717 = f32[2,512,364]{2,1,0} reshape(f32[93184,4]{1,0}%get-tuple-element.3)
-  ROOT %add = f32[2,512,364] add(%reshape.9717, %reshape.9717)
-}
-
-ENTRY main {
-  pred.1 = pred[] parameter(0)
-  arg.2 = f32[93184,4]{1,0} parameter(1)
-  arg_tuple.11 = (f32[93184,4]{1,0}) tuple(arg.2)
-  call.1 = f32[2,512,364] call(arg_tuple.11), to_apply=call_1
-  conditional = f32[2,512,364] conditional(pred.1, arg_tuple.11, arg_tuple.11),
-                     true_computation=on_true, false_computation=on_false
-  ROOT add = f32[2,512,364] add(call.1, conditional)
-}
-  )";
-
-  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> m,
-                          ParseAndReturnVerifiedModule(module_str));
-  ComputationLayout computation_layout(
-      m->entry_computation()->ComputeProgramShape());
-  *computation_layout.mutable_parameter_layout(0) =
-      ShapeLayout(ShapeUtil::MakeShape(PRED, {}));
-  *computation_layout.mutable_parameter_layout(1) =
-      ShapeLayout(ShapeUtil::MakeShapeWithDenseLayout(F32, {93184, 4}, {0, 1}));
-  *computation_layout.mutable_result_layout() = ShapeLayout(
-      ShapeUtil::MakeShapeWithDenseLayout(F32, {2, 512, 364}, {0, 1, 2}));
-  ChannelLayoutConstraints channel_constraints;
-  LayoutAssignment layout_assignment(
-      &computation_layout,
-      /*channel_constraints=*/&channel_constraints,
-      /* reverse_computation_order = */ true);
-  EXPECT_IS_OK(layout_assignment.Run(m.get()).status());
-  const HloInstruction* call_1 = FindInstruction(m.get(), "reshape.8494");
-  ExpectLayoutIs(call_1->shape(), {0, 1, 2});
-  const HloInstruction* on_true = FindInstruction(m.get(), "reshape.8493");
-  ExpectLayoutIs(on_true->shape(), {0, 1, 2});
-  const HloInstruction* on_false = FindInstruction(m.get(), "reshape.9717");
-  ExpectLayoutIs(on_false->shape(), {0, 1, 2});
-}
 
 // Test the ability to propagate operand constraints across multiple operations.
 TEST_F(LayoutAssignmentTest, PropagateOperandLayout) {
